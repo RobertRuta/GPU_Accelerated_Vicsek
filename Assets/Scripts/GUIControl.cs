ced64bd43f0594005323309dba8d1242719e04da
RobertRuta <robertruta50@gmail.com>
2023-09-29 13:13:27 +0200

    Added GUI component along with speed slider. Created functions for easily adding new sliders in the top right.

--- Assets/Scripts/GUIControl.cs
+++ Assets/Scripts/GUIControl.cs
@@ -1,36 +1,53 @@
-// using UnityEngine;
-// public class GUIControl : MonoBehaviour {
-//     SimulationControl sim;
-//     void Awake()
-//     {
-//         sim = GetComponent<SimulationControl>();
-//     }
+using UnityEngine;
+public class GUIControl : MonoBehaviour {
+    SimulationControl sim;
+    Visualiser vis;
+    
+    void Awake()
+    {
+        sim = GetComponent<SimulationControl>();
+        vis = GetComponent<Visualiser>();
+    }
 
-//     public int particleCount {get => sim.particleCount; set => sim.particleCount=value;}
-//     public float radius {get => sim.radius; set => sim.radius=value;}
-//     public float noise {get => sim.noise; set => sim.noise=value;}
-//     public float particleSize {get => sim.particleSize; set => sim.particleSize=value;}
-//     public float particleDensity {get => sim.particleDensity;}
-//     public float particleCellDensity {get => sim.particleCellDensity;}
+    public int particleCount {get => sim.particleCount; set => sim.particleCount=value;}
+    public float radius {get => sim.radius; set => sim.radius=value;}
+    public float noise {get => sim.noise; set => sim.noise=value;}
+    public float speed {get => sim.speed; set => sim.speed=value;}
+    public float particleDensity {get => sim.particleDensity;}
+    public float particleCellDensity {get => sim.particleCellDensity;}
+    public float particleSize {get => vis.particleSize; set => vis.particleSize=value;}
+
+    int labelCounter = 0;
 
 
-//     void OnGUI() {
-//         GUI.Label(new Rect(265, 15, 200, 30), "Particle Count: " + particleCount.ToString());
-//         particleCount = (int)GUI.HorizontalSlider(new Rect(25, 20, 200, 30), (float)particleCount, 1.0f, Mathf.Pow(2,21)+1);
-        
-//         // GUI.Label(new Rect(265, 45, 200, 30), "Box width: " + box_width.ToString() + "m");
-//         // box_width = GUI.HorizontalSlider(new Rect(25, 50, 200, 30), box_width, box_range.x, box_range.y);
+    void OnGUI() {
+        sim.particleCount = AddUIElement("Particle Count: ", sim.particleCount, "", 2, 1000000);
+        sim.radius = AddUIElement("Neighbour Radius: ", sim.radius, "m", 0.5f, 10f);
+        sim.speed = AddUIElement("Speed: ", sim.speed, "m/s", 0.1f, 100f);
+        sim.noise = AddUIElement("Noise: ", sim.noise, "", 0.0f, 1f);
+        vis.particleSize = AddUIElement("Particle Size: ", vis.particleSize, "", 0.5f, 5f);
 
-//         GUI.Label(new Rect(265, 45, 200, 30), "Neighbour radius: " + radius.ToString() + "m");
-//         radius = GUI.HorizontalSlider(new Rect(25, 50, 200, 30), radius, sim.radius_range.x, sim.radius_range.y);
-        
-//         GUI.Label(new Rect(265, 75, 200, 30), "Noise: " + noise.ToString() + "");
-//         noise = GUI.HorizontalSlider(new Rect(25, 80, 200, 30), noise, 0.0f, 1f);
-        
-//         GUI.Label(new Rect(265, 105, 200, 30), "Particle Size: " + particleSize.ToString() + "");
-//         particleSize = GUI.HorizontalSlider(new Rect(25, 110, 200, 30), particleSize, 0.05f, 5f);
+        GUI.Label(new Rect(800, 15, 200, 60), "Initial Particle Density \n" + particleDensity.ToString("F3") + " particles/m^3");
+        GUI.Label(new Rect(800, 75, 200, 60), "Initial Particle Cell Density \n" + particleCellDensity.ToString("F3") + " particles/cell");
 
-//         GUI.Label(new Rect(800, 15, 200, 60), "Initial Particle Density \n" + particleDensity.ToString("F3") + " particles/m^3");
-//         GUI.Label(new Rect(800, 75, 200, 60), "Initial Particle Cell Density \n" + particleCellDensity.ToString("F3") + " particles/cell");
-//     }
-// }
+        labelCounter = 0;
+    }
+
+
+    float AddUIElement(string label, float value, string unit, float min, float max) {
+        int y_pos = 15 + labelCounter*30;
+        GUI.Label(new Rect(265, y_pos, 200, 30), label + value.ToString() + unit);
+        value = GUI.HorizontalSlider(new Rect(25, y_pos + 5, 200, 30), value, min, max);
+        labelCounter++;
+        return value;
+    }
+
+
+    int AddUIElement(string label, int value, string unit, float min, float max) {
+        int y_pos = 15 + labelCounter*30;
+        GUI.Label(new Rect(265, y_pos, 200, 30), label + value.ToString() + unit);
+        value = (int)GUI.HorizontalSlider(new Rect(25, y_pos + 5, 200, 30), (float)value, min, max);
+        labelCounter++;
+        return value;
+    }
+}
